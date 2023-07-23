9efb429fab961e819b8584f8b5d205a737758d1d
ericprd <ericprd_@outlook.com>
2023-07-23 23:26:45 +0700

    test deploy

--- lib/get-env.lib.go
+++ lib/get-env.lib.go
@@ -1,18 +1,15 @@
 package lib
 
 import (
-	"log"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 func GetEnv(name string) string {
-	err := godotenv.Load()
+	// err := godotenv.Load()
 
-	if err != nil {
-		log.Fatal(err.Error())
-	}
+	// if err != nil {
+	// 	log.Fatal(err.Error())
+	// }
 
 	return os.Getenv(name)
 }
